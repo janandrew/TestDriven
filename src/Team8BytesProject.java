29a374c1569542368ad81058e4af5e7b9d5cc85e
Jandrew <Jandrew@192.168.1.101>
2016-02-11 10:04:11 +0800

    Im almost committed

---
+++ src/Team8BytesProject.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jandrew
+ */
+public class Team8BytesProject 
+{
+     /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("YOU'VE BEEN HOCK!");
+    }
+}
